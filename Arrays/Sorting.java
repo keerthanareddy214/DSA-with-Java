ffbb5b19959747a3ef917c42335e22c15b67b145
2400032589 <reddykeerthana145@gmail.com>
2026-05-13 22:53:48 +0530

    Sorting operation

---
+++ Arrays/Sorting.java
@@ -0,0 +1,15 @@
+
+package Arrays;
+import java.util.Arrays;
+
+
+public class Sorting {
+    public static void main(String[] args) {
+        int[] arr = {40,10,30,50,20};
+        Arrays.sort(arr);
+        for(int no : arr) {
+            System.out.print(no + " ");
+        }
+    }
+    
+}
